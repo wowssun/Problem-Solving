f0e8952eacd41aa5bddb59451757a5a8de2f0beb
wow_dev <119738419+wowssun@users.noreply.github.com>
2023-04-25 16:55:14 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("\\    /\\");   // 역슬래쉬 하나를 쓰기 위해서는 역슬래쉬 두개가 필요하다. \\ --> \
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		
+	}
+}
