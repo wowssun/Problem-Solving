a0e96514ae8c5b271c1e59b8d69ebbc3fc0f3082
wow_dev <119738419+wowssun@users.noreply.github.com>
2023-04-26 00:58:10 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\"); // 큰따옴표는 \" 로 표현
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+
+	}
+
+}
