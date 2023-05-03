4cf4c244b7d9cd6ac152f935a5871e411b441651
wow_dev <119738419+wowssun@users.noreply.github.com>
2023-05-03 22:33:39 +0900

    Update A＋B － 3.java

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -8,7 +8,7 @@ public class Main {
 		int x = sc.nextInt();  // 먼저 입력받을 갯수가 될 x를 입력받는다.
 // 		int a = 0,b = 0;   // 이것보단
 		
-		for(int i = 1; i <= x; i ++) {  1부터 x까지 1씩 증가 하면서
+		for(int i = 1; i <= x; i ++) { // 1부터 x까지 1씩 증가 하면서
 			int a = sc.nextInt();   // 이게 훨씬 깔끔하당
 			int b = sc.nextInt();  // a와 b를 입력받는다.
 			System.out.println(a+b);   // 입력받은 즉시 계산
