8fc6e130daf67879a119f2e28e9d029d52f670cc
wow_dev <119738419+wowssun@users.noreply.github.com>
2023-05-04 22:57:38 +0900

    Update 단어 길이 재기.java

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+import java.util.Scanner;    // 스캐너로 입력받고
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String a = sc.next();
+		String a = sc.next();   //스트링타입으로 입력받는다.
 		
-		String [] al = new String [a.length()];
+		String [] al = new String [a.length()];   //배열 괜히 했다. 이거 말고
+	     // System.out.println(a.length());   이렇게 해주면 된다.  
 		
-		System.out.println(al.length);	
+		System.out.println(al.length);	// 여기서 a.length가 오류가 났던 이유는 배열은 길이를 .length로 받고 스트링은 .length() 으로 받는다.
+									// 추가로 .size로 크기를 받는 것도 있다.
 	}
 
 }
