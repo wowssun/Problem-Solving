f5593b11e9cd9e749f01ba0d20cd76286824317d
wow_dev <119738419+wowssun@users.noreply.github.com>
2023-12-20 22:53:46 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 460 ms, Memory: 18520 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		sc.close();
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N-i; j++) {
+				System.out.print(" ");
+			}
+			for(int a = 0; a < i; a++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
